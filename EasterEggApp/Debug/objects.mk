105799e9849b392f5e9ab17ced67013612602dde
Michael Schmidl <mschmidl@ultratronik.de>
2021-01-23 14:30:10 +0100

    removed more generated files. ATTENTION: open the RTECONFIG file first to ensure generation of all dependencies

--- EasterEggApp/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := C:/Users/mschmidl/ON_Semiconductor/PACK/ONSemiconductor/RSL10/3.4.154/lib/Release/libfota.a C:/Users/mschmidl/ON_Semiconductor/PACK/ONSemiconductor/RSL10/3.4.154/lib/ble_profiles/Release/libbass.a C:/Users/mschmidl/ON_Semiconductor/PACK/ONSemiconductor/RSL10/3.4.154/lib/ble_profiles/Release/libdiss.a C:/Users/mschmidl/ON_Semiconductor/PACK/ONSemiconductor/RSL10/3.4.154/lib/ble_profiles/Release/libhogpd.a
-
-LIBS :=
-
